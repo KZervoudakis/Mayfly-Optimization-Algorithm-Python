9f3028ad387aa85c50c5fc6df79638b11076a569
Konstantinos Zervoudakis <59539784+KZervoudakis@users.noreply.github.com>
2023-04-18 13:06:57 +0300

    Delete printingIter.py

--- printingIter.py
+++
@@ -1,4 +0,0 @@
-def printingperiter(problem,it,gbest,namemethod,funcevals,curtrial):
-
-    print('Problem: {}, Method: {}, Trial: {}, Iteration: {}, Function Evaluations: {}, Best Cost = {}'.format(problem['CostFunction'].__name__,namemethod,curtrial+1,it, funcevals, gbest['cost']))
-    return
